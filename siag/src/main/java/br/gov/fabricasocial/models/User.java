5e0ec7c7da296b760e0f0d4490e2266ef2ab0e5c
msfernandes <matheus.souza.fernandes@gmail.com>
2014-05-05 17:38:18 -0300

    Criando model User

---
+++ siag/src/main/java/br/gov/fabricasocial/models/User.java
@@ -0,0 +1,29 @@
+package br.gov.fabricasocial.models;
+
+public class User {
+	
+	int idUser;
+	String username;
+	
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
+	
+	
+}
