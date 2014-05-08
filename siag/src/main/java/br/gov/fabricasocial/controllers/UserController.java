5e2e73b3143821f83654828926c90d3e14957096
msfernandes <matheus.souza.fernandes@gmail.com>
2014-05-08 08:30:10 -0300

    Criação da controller login

--- siag/src/main/java/br/gov/fabricasocial/controllers/UserController.java
+++ siag/src/main/java/br/gov/fabricasocial/controllers/UserController.java
@@ -1,10 +1,17 @@
 package br.gov.fabricasocial.controllers;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import javax.naming.AuthenticationException;
 import javax.naming.NamingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.gov.fabricasocial.dao.UserDAO;
 import br.gov.fabricasocial.dao.jdbc.JdbcUserDAO;
@@ -12,7 +19,21 @@ import br.gov.fabricasocial.dao.ldap.LdapAuth;
 import br.gov.fabricasocial.models.User;
 
 public class UserController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class); 
+	
 	private static int USUARIO_CADASTRO = 1;
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		UserDAO dao = new JdbcUserDAO();
+		dao.setUserLogin("Cadastro", "cadastro");
+		
+		String localUsername = System.getProperty("user.name");
+		
+		model.addAttribute("localUsername", localUsername);
+		
+		return "login";
+	}
 	
 	public static void main(String[] args) {
 		UserDAO dao = new JdbcUserDAO();
