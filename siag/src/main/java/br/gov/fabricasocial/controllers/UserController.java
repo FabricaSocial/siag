79aa85f3dc69ce7bdb390445548f46bfc435dc59
msfernandes <matheus.souza.fernandes@gmail.com>
2014-05-08 08:40:12 -0300

    Adicionando arquivos estaticos

--- siag/src/main/java/br/gov/fabricasocial/controllers/UserController.java
+++ siag/src/main/java/br/gov/fabricasocial/controllers/UserController.java
@@ -9,6 +9,7 @@ import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import br.gov.fabricasocial.dao.jdbc.JdbcUserDAO;
 import br.gov.fabricasocial.dao.ldap.LdapAuth;
 import br.gov.fabricasocial.models.User;
 
+@Controller
 public class UserController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class); 
 	
@@ -33,48 +35,5 @@ public class UserController {
 		model.addAttribute("localUsername", localUsername);
 		
 		return "login";
-	}
-	
-	public static void main(String[] args) {
-		UserDAO dao = new JdbcUserDAO();
-		dao.setUserLogin("Cadastro", "cadastro");
-		
-		String localUsername = System.getProperty("user.name");
-		System.out.println("Usuário logado na máquina: " + localUsername);
-		
-		List<User> users = dao.findByUserName(localUsername);
-		User user;
-		
-		String mensagem = "";
-		
-		if(users.size() != 0) {
-			user = users.get(0);
-		} else {
-			System.out.println("\nPrimeiro Acesso!\nDigite sua senha: ");
-				
-			@SuppressWarnings("resource")
-			Scanner scanner = new Scanner(System.in);
-			String password = scanner.nextLine();
-				
-			user = new User(localUsername, password, USUARIO_CADASTRO);				
-		}
-		
-		try {
-			LdapAuth ldapAuth = new LdapAuth();
-			
-			ldapAuth.authenticateUser(user);
-			dao.insert(user);
-			
-			mensagem = "Autenticado com sucesso!";
-		} catch (AuthenticationException authEx) {
-			authEx.getCause().printStackTrace();
-			
-			mensagem = "Erro na autenticação!";
-		} catch (NamingException namEx) {
-			mensagem = "Problema na conexão!";
-			namEx.getCause().printStackTrace();
-		}
-		
-		System.out.println(mensagem);
 	}
 }
